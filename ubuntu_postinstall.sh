65734deb3a00a58a07ca72217f144fdb41ed00dc
Nicolas Bonnand <40770542+nbonnand@users.noreply.github.com>
2018-11-02 13:05:41 +0100

    Update ubuntu_postinstall.sh

--- ubuntu_postinstall.sh
+++ ubuntu_postinstall.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y software-properties-common apt-transport-https
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list 
+sudo bash -c "echo \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" > /etc/apt/sources.list.d/docker.list"
 
 sudo apt-get -y update
 
