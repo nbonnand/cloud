1fd51b1688eb247dcdc089dd8ff177618d67b250
Nicolas Bonnand <40770542+nbonnand@users.noreply.github.com>
2018-11-02 14:31:23 +0100

    Update ubuntu_postinstall.sh

--- ubuntu_postinstall.sh
+++ ubuntu_postinstall.sh
@@ -8,8 +8,22 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo bash -c "echo \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" > /etc/apt/sources.list.d/docker.list"
 
-sudo apt-get -y update
+# give a grace period for package installation
 
-sudo apt-get install -y docker-ce openvswitch-switch plotnetcfg
+keep_running=6
+while [ $keep_running -ne 0 ]
+do
+	if sudo apt-get install -y docker-ce openvswitch-switch plotnetcfg
+	then
+        	echo 'yes'
+        	keep_running=0
+    	else
+        	echo 'no'
+		keep_running=$(( keep_running - 1 ))
+		sudo apt-get -y update
+	fi
+    	sleep 10
+    	echo "postinstall keep_running=$keep_running"
+done
 
-
+echo "done"
